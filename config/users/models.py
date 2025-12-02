4a562a36e441fcd2ded040f852f0f3125182e72b
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-12-02 21:43:01 +0330

    update users app

--- config/users/models.py
+++ config/users/models.py
@@ -59,17 +59,19 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
-class Follow(models.Model):
-    follower = models.ForeignKey(User, related_name='following_set', on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name='followers_set', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        unique_together = ('follower', 'following')
-        indexes = [
-            models.Index(fields=['follower']),
-            models.Index(fields=['following']),
-        ]
 
-    def __str__(self):
-        return f"{self.follower.username} → {self.following.username}"
+# class Follow(models.Model):
+#     follower = models.ForeignKey(User, related_name='following_set', on_delete=models.CASCADE)
+#     following = models.ForeignKey(User, related_name='followers_set', on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         unique_together = ('follower', 'following')
+#         indexes = [
+#             models.Index(fields=['follower']),
+#             models.Index(fields=['following']),
+#         ]
+
+#     def __str__(self):
+#         return f"{self.follower.username} → {self.following.username}"
