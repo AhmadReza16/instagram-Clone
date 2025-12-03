c162999367d1c340f9a78565d0261b7cc81e2671
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-12-03 23:22:01 +0330

    create serialaizers for likes app

---
+++ config/likes/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Like
+from posts.serializers import PostSerializer  # read-only use of post data
+
+User = get_user_model()
+
+
+class SimpleUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "full_name", "profile_image")
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = SimpleUserSerializer(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=None, required=True)  # set queryset in view
+
+    class Meta:
+        model = Like
+        fields = ("id", "user", "post", "created_at")
+        read_only_fields = ("id", "user", "created_at")
