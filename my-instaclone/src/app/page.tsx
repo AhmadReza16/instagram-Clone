48edabb1d2fdc4c5d93685ac4e1a07fedbc62aca
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-12-12 21:54:26 +0330

    update code and add packages

--- my-instaclone/src/app/page.tsx
+++ my-instaclone/src/app/page.tsx
@@ -1,10 +1,13 @@
+import Hero from "@/components/home/Hero";
+import Features from "@/components/home/Features";
+import CTA from "@/components/home/CTA";
+
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-semibold">
-        Welcome to Instaclone (Next + TS + Tailwind)
-      </h1>
-      <p className="mt-2 text-gray-600">Phase 1 scaffold up and running.</p>
-    </div>
+    <main>
+      <Hero />
+      <Features />
+      <CTA />
+    </main>
   );
 }
