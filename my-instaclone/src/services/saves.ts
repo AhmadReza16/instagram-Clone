4f69073970a436b9e018724f8df123ce1684c265
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2026-02-05 22:55:19 +0330

    update fix saves.ts in services

--- my-instaclone/src/services/saves.ts
+++ my-instaclone/src/services/saves.ts
@@ -1,11 +1,11 @@
 import { apiClient } from "@/lib/api-client";
 
 export async function savePost(postId: number) {
-  const { data } = await apiClient.post(`/posts/${postId}/save/`);
+  const { data } = await apiClient.post(`/save/${postId}/`);
   return data;
 }
 
 export async function unsavePost(postId: number) {
-  const { data } = await apiClient.delete(`/posts/${postId}/save/`);
+  const { data } = await apiClient.delete(`/unsave/${postId}/`);
   return data;
 }
