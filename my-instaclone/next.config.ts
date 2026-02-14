744921f309d33fcd061348aa47608a6b5f06df67
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2026-02-14 21:37:44 +0330

    update next.config and packages

--- my-instaclone/next.config.ts
+++ my-instaclone/next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
